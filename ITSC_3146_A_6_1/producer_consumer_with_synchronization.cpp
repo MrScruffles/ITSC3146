4eeaf041205822a3395c56e5b7eb98f69ce98e51
Ash <ash@mountaininteractive.com>
2024-02-13 09:44:16 -0500

    bruh fixk this

--- ITSC_3146_A_6_1/producer_consumer_with_synchronization.cpp
+++ ITSC_3146_A_6_1/producer_consumer_with_synchronization.cpp
@@ -12,107 +12,97 @@ using namespace std;
 // that has an order number and an item number.
 struct Order
 {
-	int order_num;
-	int item_num;	
+    int order_num;
+    int item_num;	
 };
 
 Order new_orders [N];		// array of elements of type Order to be used as a shared buffer
 int num_new_orders = 0;		// count of number of new (i.e., unprocessed) orders
 int order_num = 0;			// global variable used to generate unique order numbers
 
-// TODO: Define and initialize necessary mutex and condition variables here
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+// Define and initialize necessary mutex and condition variables
+pthread_mutex_t data_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t console_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond_full = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond_empty = PTHREAD_COND_INITIALIZER;
 
 void* takeOrders(void* arg)
 {
-	int item;
-	int index = 0;
-	
-	for(int i = 0; i < MAX; ++i) {
-		// Beginning of critical region 1
-		
-        // Get user input
-		cout << "Enter a menu item number between 1 and 50: ";
-		cin >> item;
+    int item;
+    int index = 0;
+    
+    for(int i = 0; i < MAX; ++i) {
+        cout << "Enter a menu item number between 1 and 50: ";
+        cin >> item;
 
-        // Print new order's details
-		cout << "Got new order! Order number is " << order_num << " and item number: " << item << std::endl;
-		
-		// End of critical region 1
-		
-	
-		// Beginning of critical region 2
+        pthread_mutex_lock(&data_mutex);
+        while(num_new_orders == N)
+            pthread_cond_wait(&cond_full, &data_mutex);
 
-        // Put new order into new orders buffer and update number of new orders
         new_orders[index].order_num = order_num;
-	    new_orders[index++].item_num = item;
-    	++num_new_orders;
-    	
-    	// End of critical region 2
+        new_orders[index++].item_num = item;
+        ++num_new_orders;
+        ++order_num;
+        if(index == N)
+            index = 0;
 
-    	
-    	// Update order number so that next order gets a different number
-    	++order_num;
-    	
-    	// If the end of the new orders buffer is reached, wrap back around
-    	if(index == N)
-    		index = 0;
-	}
+        pthread_cond_signal(&cond_empty);
+        pthread_mutex_unlock(&data_mutex);
 
-	pthread_exit(NULL);
+        pthread_mutex_lock(&console_mutex);
+        cout << "Got new order! Order number is " << order_num - 1 << " and item number: " << item << std::endl;
+        pthread_mutex_unlock(&console_mutex);
+
+        sleep(1);  // Introduce a small delay
+    }
+
+    pthread_exit(NULL);
 }
 
 void* processOrders(void* arg)
 {
-	int item;
-	int index = 0;
-	int o_num;
+    int item;
+    int index = 0;
+    int o_num;
 
-	for(int i = 0; i < MAX; ++i) {
-		// Beginning of critical region 3
+    for(int i = 0; i < MAX; ++i) {
+        pthread_mutex_lock(&data_mutex);
+        while(num_new_orders == 0)
+            pthread_cond_wait(&cond_empty, &data_mutex);
 
-        // Retrieve new order details from buffer and update number of new orders
         o_num = new_orders[index].order_num;
-	    item = new_orders[index++].item_num;
+        item = new_orders[index++].item_num;
         --num_new_orders;
 
-		// End of critical region 3
+        if(index == N)
+            index = 0;
 
-		
-		// Beginning of critical region 4
-            
-        // Print retrieved order's details
-		cout << "Processing order number " << o_num << " with item number: " << item << std::endl;
+        pthread_cond_signal(&cond_full);
+        pthread_mutex_unlock(&data_mutex);
 
-		// End of critical region 4
+        sleep(1);  // Introduce a small delay
 
+        pthread_mutex_lock(&console_mutex);
+        cout << "Processing order number " << o_num << " with item number: " << item << std::endl;
+        pthread_mutex_unlock(&console_mutex);
+    }
 
-		// Suspend self for 1 second
-		sleep(1);
-		
-    	// If the end of the new orders buffer is reached, wrap back around
-    	if(index == N)
-    		index = 0;
-	}
-
-	pthread_exit(NULL);
+    pthread_exit(NULL);
 }
 
 int main()
 {
     // Create threads to take and process orders
-	pthread_t id1, id2;
-	pthread_create(&id1, NULL, processOrders, NULL);
-	pthread_create(&id2, NULL, takeOrders, NULL);
-	
-	// TODO: Add code to wait for both threads to finish
-	pthread_join(id1, NULL);
-	pthread_join(id2, NULL);
+    pthread_t id1, id2;
+    pthread_create(&id2, NULL, takeOrders, NULL);  // Create takeOrders thread first
+    pthread_create(&id1, NULL, processOrders, NULL);  // Then create processOrders thread
+    
+    // Wait for both threads to finish
+    pthread_join(id1, NULL);
+    pthread_join(id2, NULL);
 
-	
-	// Print goodbye message
-	cout << "Phew! Done with orders for today!" << endl;
+    // Print goodbye message
+    cout << "Phew! Done with orders for today!" << endl;
 
-	pthread_exit(NULL);
+    pthread_exit(NULL);
 }
